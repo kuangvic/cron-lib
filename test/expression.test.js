bac41024defaac343c2e35c4d1f3c15e8218aaa0
vic.kuang <vic.kuang@qiyejia360.com>
2018-09-13 17:31:55 +0800

    add *|number expression to customize business

---
+++ test/expression.test.js
@@ -0,0 +1,16 @@
+const Parser = require('../lib/parser')
+const moment = require('moment-timezone')
+
+const parser = new Parser(`0 0 0 *|12 * *`, {
+  startDate: moment().format('YYYY-MM-DD HH:mm:ss'),
+  endDate: moment().add(5, 'years').format('YYYY-MM-DD HH:mm:ss')
+})
+
+console.log(parser.getExpression())
+
+// get all scheduled time
+while (true) {
+  const matched = parser.next()
+  if (!matched) break
+  console.log(moment(matched).format('YYYY-MM-DD HH:mm:ss'))
+}
